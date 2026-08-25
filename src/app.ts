31bfbe3d57aa9c058baa67a8dec277ba52620a10
shanbel <shanbeldires7@gmail.com>
2026-08-25 10:23:52 +0300

    create the root app

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import eventRoutes from "./routes/event.routes.js";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+
+app.use("/api/events", eventRoutes);
+
+export default app;
