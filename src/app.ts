ddf2e94a6753f03a6170271ea0976eb7377e86ea
Shanbel Dires <shanbeldires7@gmail.com>
2026-08-28 19:15:51 +0300

    import kiflat route in to root app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 
 import eventRoutes from "./routes/event.routes.js";
+import kiflatRoutes from "./routes/kiflat.route";
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 app.use("/api/events", eventRoutes);
+app.use("/api/kiflats", kiflatRoutes);
 
 export default app;
