aa7762bd654bf4ab5f3e018d219e9dcb6c9a1a60
shanbel <shanbeldires7@gmail.com>
2026-08-28 17:19:21 +0300

    add magazine routes

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 
 import eventRoutes from "./routes/event.routes.js";
-
+import magazineRoutes from "./routes/magazine.route.js";
 const app = express();
 
 app.use(helmet());
@@ -19,5 +19,5 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 app.use("/api/events", eventRoutes);
-
+app.use("/api/magazines",magazineRoutes);
 export default app;
