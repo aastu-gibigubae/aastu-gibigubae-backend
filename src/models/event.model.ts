a5cc2e9c3e7a783661eb7cce22ed2430e7cf876e
shanbel <shanbeldires7@gmail.com>
2026-08-28 17:44:04 +0300

    add event query method models

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -18,6 +18,11 @@ export interface UpdateEventInput {
 }
 
 export interface EventQuery {
-  page?: number;
-  limit?: number;
+  page: number;
+  limit: number;
+  search?: string;
+  fromDate?: Date;
+  toDate?: Date;
+  sortBy: "event_date" | "title" | "created_at";
+  sortOrder: "asc" | "desc";
 }
